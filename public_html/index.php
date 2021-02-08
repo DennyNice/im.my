7e7300a04879f3d5bf600b99010edc6565c58937
DennyNice <dennynice@icloud.com>
2021-02-09 00:18:49 +0100

    up

--- public_html/index.php
+++ public_html/index.php
@@ -12,5 +12,6 @@ session_start();
 # В config.php будут хранится базовые настройки необходимые для быстрого развертывания сайта на хостинге
 require_once "config.php";
 
-# В internal_settings.php будут хранится более фундаменталтыне настройки например: пути к шаблонам, настройки безапасности сайта
+//TODO add internal_settings.php
+# В internal_settings.php будут хранится более фундаментальные настройки например: пути к шаблонам, настройки безапасности сайта
 require_once "core/base/settings/internal_settings.php";
